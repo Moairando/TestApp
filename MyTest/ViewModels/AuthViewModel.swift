8ec52dd83b58645152f1f65ed21b578f10534590
c0a2310239 <c0a2310239@edu.teu.ac.jp>
2025-06-11 14:30:09 +0900

    フィードバックの取得を変更
    フィードバック取得のHandlerをasync/awaitに変更

--- MyTest/ViewModels/AuthViewModel.swift
+++ MyTest/ViewModels/AuthViewModel.swift
@@ -17,19 +17,30 @@ class AuthViewModel {
     
     //Create Account
     
-    func createAccount(email: String, password: String) {
+    func createAccount(email: String, password: String) async {
 //        Auth.auth().createUser(withEmail: email, password: password)
-        Auth.auth().createUser(withEmail: email, password: password) { result, error in
-            print("Firebaseサーバーからの結果が通知されました")
+//        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+//            print("Firebaseサーバーからの結果が通知されました")
+//            
+//            if let error = error {
+//                print("ユーザー登録失敗: \(error.localizedDescription)")
+//            }
+//            
+//            if let result = result {
+//                print("ユーザー登録成功: \(result.user.email)")
+//            }
+//        }
+        
+        do {
+            let result = try await Auth.auth().createUser(withEmail: email, password: password)
+            print("ユーザー登録成功: \(result.user.email)")
             
-            if let error = error {
-                print("ユーザー登録失敗: \(error.localizedDescription)")
-            }
-            
-            if let result = result {
-                print("ユーザー登録成功: \(result.user.email)")
-            }
+        }catch {
+            print("ユーザー登録失敗: \(error.localizedDescription)")
         }
+        
+        
+        
         print("アカウント確認画面から呼び出されました")
     }
     

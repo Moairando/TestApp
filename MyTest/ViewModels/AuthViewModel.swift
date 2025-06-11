e1916a9659404b7a2fdccd490b93f4750a678390
c0a2310239 <c0a2310239@edu.teu.ac.jp>
2025-06-11 13:46:09 +0900

    Firebase認証のフィードバックの取得

--- MyTest/ViewModels/AuthViewModel.swift
+++ MyTest/ViewModels/AuthViewModel.swift
@@ -18,7 +18,18 @@ class AuthViewModel {
     //Create Account
     
     func createAccount(email: String, password: String) {
-        Auth.auth().createUser(withEmail: email, password: password)
+//        Auth.auth().createUser(withEmail: email, password: password)
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            print("Firebaseサーバーからの結果が通知されました")
+            
+            if let error = error {
+                print("ユーザー登録失敗: \(error.localizedDescription)")
+            }
+            
+            if let result = result {
+                print("ユーザー登録成功: \(result.user.email)")
+            }
+        }
         print("アカウント確認画面から呼び出されました")
     }
     

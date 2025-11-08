bfe27377f97b5d3381feb984a8ba1f510e3ece9d
c0a2310239 <c0a2310239@edu.teu.ac.jp>
2025-11-08 15:23:15 +0900

    ListViewの構造の修正

--- MyTest/Views/List/ListView.swift
+++ MyTest/Views/List/ListView.swift
@@ -11,6 +11,7 @@ struct ListView: View {
     @ObservedObject private var viewModel = ListViewModel()
     
     var body: some View {
+<<<<<<< Updated upstream
         VStack(spacing: 0) {
             
             //Cards
@@ -23,6 +24,52 @@ struct ListView: View {
         }
         .background(.black, in: RoundedRectangle(cornerRadius: 15))
         .padding(.horizontal, 6)
+=======
+        NavigationStack {
+            Group {
+                if viewModel.users.count > 0 {
+                    VStack(spacing: 0) {
+                        
+                        //Cards
+                        cards
+                        
+                        
+                        //Actions
+                        actions
+                        
+                    }
+                    .background(.black, in: RoundedRectangle(cornerRadius: 15))
+                    .padding(.horizontal, 6)
+                } else {
+                    ProgressView()
+                        .padding()
+                        .tint(Color.white)
+                        .background(Color.gray)
+                        .clipShape(RoundedRectangle(cornerRadius: 6))
+                        .scaleEffect(1.5)
+                }
+            }
+            .navigationTitle("Fire Match")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    BrandImage(size: .small)
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink {
+                        MyPageView()
+                    } label: {
+                        Image("SwiftUI Firebase Course Avatar")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 32, height: 32)
+                            .clipShape(.circle)
+                    }
+                }
+            }
+        }
+        .tint(.primary)
+>>>>>>> Stashed changes
     }
 }
 
